0721e28fa85aad5ee95e203733d6fd592e854a9a
Gachuhimartin <martingachuhimaina@gmail.com>
2025-08-22 17:34:25 +0300

    assignments wk 4

--- week3/assignment.py
+++ week3/assignment.py
@@ -14,7 +14,7 @@ discount_percent = float(input("Enter the discount percentage: "))
 
 # Calculate final price
 final_price = calculate_discount(price, discount_percent)
-
+ 
 # Print result
 if discount_percent >= 20:
     print(f"The final price after a {discount_percent}% discount is: {final_price:.2f}")
